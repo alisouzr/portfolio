2bc2dfcf239af306691a28a02ac68f15a81ba8e5
alisouzr <79667413+alisouzr@users.noreply.github.com>
2023-12-12 14:15:44 +0000

    background image fade out

--- script.js
+++ script.js
@@ -3,3 +3,7 @@ window.addEventListener("scroll", function() {
   header.classList.toggle("sticky", window.scrollY > 0);
 })
 
+window.addEventListener("scroll", function() {
+  var work = document.querySelector(".work");
+  work.style.opacity = 1 - window.scrollY / 500;
+})
